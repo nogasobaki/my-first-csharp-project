b94177074abdb1354a90268e1abda165eba45c1e
ногасобаки <ismailovhazar2@gmail.com>
2026-01-31 21:13:13 +0300

    имправил баг с делением на ноль

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace projekt{
+    
+     class Program
+    {
+       static void Main() 
+       { System.Console.WriteLine(" ВВЕДИТЕ ПЕРВОЕ ЗНАЧЕНИЕ : ");
+        
+        float x;
+
+       x=float.Parse(Console.ReadLine());
+
+       System.Console.WriteLine(" ВВЕДИТЕ ДЕЙСТВИЕ + , - , * , /  ");
+
+       string a; 
+
+       a=Convert.ToString(Console.ReadLine());
+
+       System.Console.WriteLine(" ВВЕДИТЕ ВТОРОЕ ЗНАЧЕНИЕ : ");
+
+       float y;
+       
+       y=float.Parse(Console.ReadLine());
+
+      if (a == "+") {
+
+          System.Console.WriteLine( $"ВАШ ОТВЕТ : {x + y} "); 
+          
+          }
+
+          else if ( a == "-")
+            {
+               System.Console.WriteLine( $"ВАШ ОТВЕТ : {x - y} " ); 
+
+            }
+            else if ( a == "*")
+            {
+                System.Console.WriteLine( $"ВАШ ОТВЕТ : {x * y} ");
+
+            }
+            else if (a == "/" && y == 0 )
+            {
+                
+                System.Console.WriteLine(" НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯ!");
+            }
+
+
+
+            else if ( a == "/")
+            {
+                System.Console.WriteLine( $"ВАШ ОТВЕТ : {x / y}" );
+
+
+            
+
+            }
+
+
+
+
+            
+
+
+        }
+
+
+    }
+    
+}

059581ca27aafd562d76c18d8a0e08c48994f5a3
ногасобаки <ismailovhazar2@gmail.com>
2026-02-12 15:25:32 +0300

    Improved calculator input validation

--- Program.cs
+++ Program.cs
@@ -1,55 +1,104 @@
 ﻿using System;
-namespace projekt{
-    
+namespace project
+{
+
      class Program
-    {
-       static void Main() 
-       { System.Console.WriteLine(" ВВЕДИТЕ ПЕРВОЕ ЗНАЧЕНИЕ : ");
-        
-        float x;
+     {
+          static void Main()
+          {
+               while(true){
 
-       x=float.Parse(Console.ReadLine());
+               try
+               {
+                  
+               System.Console.WriteLine("\n\n=== КОНСОЛЬНЫЙ КАЛЬКУЛЯТОР === \n\n");
+               double x;
+               System.Console.WriteLine("== ВВЕДИТЕ ПЕРВОЕ ЗНАЧЕНИЕ : ==");
+               x = Double.Parse(Console.ReadLine());
 
-       System.Console.WriteLine(" ВВЕДИТЕ ДЕЙСТВИЕ + , - , * , /  ");
+               string a;
+               System.Console.WriteLine("\n== ВВЕДИТЕ ДЕЙСТВИЕ ==");
+               a = Console.ReadLine();
 
-       string a; 
+               double y;
+               System.Console.WriteLine("\n== ВВЕДИТЕ ВТОРОЕ ЗНАЧЕНИЕ : ==");
+               y = Double.Parse(Console.ReadLine());
 
-       a=Convert.ToString(Console.ReadLine());
+               double result = 0;
+               switch (a)
+               {
+                    case "+":
+                         result = x + y;
+                         break;
 
-       System.Console.WriteLine(" ВВЕДИТЕ ВТОРОЕ ЗНАЧЕНИЕ : ");
+                    case "-":
+                         result = x - y;
+                         break;
 
-       float y;
-       
-       y=float.Parse(Console.ReadLine());
-      
-       switch (a) {
+                    case "*":
+                         result = x * y;
+                         break;
 
-           case "+" : 
-            System.Console.WriteLine($"ВАШ ОТВЕТ : {x + y } ");
-             break;
-           case "-" : 
-           System.Console.WriteLine($"ВАШ ОТВЕТ : {x - y } ");
-            break;
-          case "*" : 
-           System.Console.WriteLine($"ВАШ ОТВЕТ : {x * y } ");
-            break;
-            case "/"  : 
-           System.Console.WriteLine($"ВАШ ОТВЕТ : {x / y } ");
-            if ( y == 0)
-                    {
-                       System.Console.WriteLine(" НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯ!"); 
+                    case "/":
+                         if (y == 0){
+                              System.Console.WriteLine("ДЕЛИТЬ НА НОЛЬ НЕЛЬЗЯ !!! ");
+
+                           continue;
+                                        
+                                   }
+                                   result = x / y; 
+                       
+                         break;
+
+               }
+               System.Console.WriteLine(" \n\n ВАШ ГРЁБАННЫЙ ОТВЕТ: " + result);
+                System.Console.WriteLine("ВВЕДИТЕ 'надоел' ЧТОБЫ ВЫЙТИ ИЛИ НАЖМИТЕ  'ENTER' ЧТОБЫ ПРОДОЛЖИТЬ ");  
+                    string exit = Console.ReadLine();
+                    if (exit == "надоел")
+                    break;
+               }
+               catch (FormatException)
+               {
+                    System.Console.WriteLine(" \n\n\n БРАТИШ ТЫ ЧОТО НЕ ТО ВВОДИШЬ , ЗАНОГО СДЕЛАЙ ДА ");
+                    
+                    
                     }
-                    break; 
-            
-            }
+                    
+              
+               }
+             
 
 
-            
+              
 
 
-        }
 
 
-    }
-    
+
+
+
+
+          }
+
+
+
+     }
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
